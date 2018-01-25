a095659e5e3f2d30ebfb219771b703a9b68f7a7b
Alex Williams <awilliams261@gmail.com>
2018-01-25 11:00:23 +0000

    Remove default CSS file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+// import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -18,18 +18,17 @@ const TemplateWrapper = ({ children }) => (
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
+        maxWidth: 700,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
-      }}
-    >
+      }}>
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
