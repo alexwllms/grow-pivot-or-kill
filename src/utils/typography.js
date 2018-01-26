ba164ecaaf18ef2e7a8cbeaabbe151c92b17e33f
Alex Williams <awilliams261@gmail.com>
2018-01-26 07:54:44 +0000

    Add Google Fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,6 +4,15 @@ const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: '1.4',
   scaleRatio: 2,
+  headerFontFamily: ['Lora', 'Helvetica', 'Arial', 'serif'],
+  headerWeight: 400,
+  bodyFontFamily: ['Lora', 'serif'],
+  googleFonts: [
+    {
+      name: 'Lora',
+      styles: ['400', '400i', '700'],
+    },
+  ],
 });
 
 export default typography;
